d4d6daa301aaf98703686b254c767a6481d2d71a
a-klimovich <kom9toshka@gmail.com>
2023-04-03 17:36:11 +0200

    add lazy move to body screpts

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 (function () {
+  $(".lazy").lazy();
+
   $(document).ready(function () {
     const clientHeaderHeight = $("header")[0].clientHeight;
     window.onscroll = () => scrollFunction();
@@ -38,118 +40,113 @@
         $shortInfo.toggleClass(cssClassOpen);
       }
       $burger.toggleClass("burger__active");
-      $searchInput.toggleClass('active');
-      $logoImage.toggleClass('is-hidden');
+      $searchInput.toggleClass("active");
+      $logoImage.toggleClass("is-hidden");
       $headerNav.toggleClass(cssClassOpen);
-      $('body').toggleClass(cssClassOpen);
+      $("body").toggleClass(cssClassOpen);
     });
 
-    const $searchInput = $('.form__label');
-    const $logoImage = $('.logo a');
+    const $searchInput = $(".form__label");
+    const $logoImage = $(".logo a");
 
     $submenuHeaderBtn.on("click", () => {
       if ($burger.hasClass("burger__active")) {
         $burger.toggleClass("burger__active");
         $headerNav.toggleClass(cssClassOpen);
       }
-      $searchInput.toggleClass('active');
+      $searchInput.toggleClass("active");
       $shortInfo.toggleClass(cssClassOpen);
-      $logoImage.toggleClass('is-hidden');
+      $logoImage.toggleClass("is-hidden");
     });
 
-    $('.nav__item .link-box').on('click', function() {
+    $(".nav__item .link-box").on("click", function () {
       $(this).offsetParent().toggleClass(cssClassActive);
     });
 
-    $('.nav__item').each(function() {
-      const hasUlInside = $(this).find('ul').length > 0;
+    $(".nav__item").each(function () {
+      const hasUlInside = $(this).find("ul").length > 0;
 
       if (!hasUlInside) {
-        $(this).find('.link-box > button').css("display", "none");
+        $(this).find(".link-box > button").css("display", "none");
       }
       console.log(hasUlInside);
     });
 
     // card open
-    const questionsCard = $(".questions-card")
-    
+    const questionsCard = $(".questions-card");
+
     questionsCard.each((i, elem) => {
       const btnOpenAnswer = $(".btnOpenAnswer", elem);
-      // const contentAnswer = $(".content__answer", elem);
-      
-
       btnOpenAnswer.on("click", () => {
         questionsCard.removeClass(cssClassActive);
         $(elem).toggleClass(cssClassActive);
-        // btnOpenAnswer.toggleClass(cssClassActive);
-        // contentAnswer.toggleClass(cssClassIsHidden);
-        // contentAnswer.siblings(".content__answer").addClass(cssClassIsHidden);
       });
-    })
+    });
 
     // PRODUCT CARD LIST ITEM COUNTER
-    const $productListCard = $('.production-list-card');
+    const $productListCard = $(".production-list-card");
 
-    $productListCard.each((i, elem)  => {
-      const listLength = $('.list-menu li', elem).length;
-      const $counterNode = $('.counter', elem);
-      
-      
-      $(elem).on('click', () => {
-        $(elem).toggleClass(cssClassActive)
-      })
+    $productListCard.each((i, elem) => {
+      const listLength = $(".list-menu li", elem).length;
+      const $counterNode = $(".counter", elem);
+
+      $(elem).on("click", () => {
+        $(elem).toggleClass(cssClassActive);
+      });
 
       if (listLength === 0) {
-        $('.list-menu', elem).remove()
-        $('.list-item-counter', elem).remove()
+        $(".list-menu", elem).remove();
+        $(".list-item-counter", elem).remove();
       }
 
       $counterNode[0].innerHTML = `${listLength}`;
-    })
+    });
 
     // SECTION VIEW CONTROLER
-    const plantScheam = $('.about-plant__scheam');
-    const linesElem = $('.scheam__lines .line')
-    const $aboutPlantContentTitle = $('.about-plant__content .title')
-    const $aboutPlantContentText = $('.about-plant__content .text')
-    const $aboutPlantContentLinkHref = $('.about-plant__content .btn-learn-more')
+    const plantScheam = $(".about-plant__scheam");
+    const linesElem = $(".scheam__lines .line");
+    const $aboutPlantContentTitle = $(".about-plant__content .title");
+    const $aboutPlantContentText = $(".about-plant__content .text");
+    const $aboutPlantContentLinkHref = $(".about-plant__content .btn-learn-more");
 
     const scheamContetn = [
       {
-        title: 'О ЗАВОДЕ',
-        text: 'Работаем с 1939 <br><br> Открытое акционерное общество «Кобринский маслодельно-сыродельный завод» это предприятие с 80-летним опытом работы и уникальными традициями производства. Мы с гордостью можем вспомнить каждый год работы завода. Нам есть, о чем рассказать и чем гордиться!',
-        href: '#a',
+        title: "О ЗАВОДЕ",
+        text:
+          "Работаем с 1939 <br><br> Открытое акционерное общество «Кобринский маслодельно-сыродельный завод» это предприятие с 80-летним опытом работы и уникальными традициями производства. Мы с гордостью можем вспомнить каждый год работы завода. Нам есть, о чем рассказать и чем гордиться!",
+        href: "#a",
       },
       {
-        title: 'ПРОИЗВОДСТВО',
-        text: 'Работаем с 1939 <br><br> Открытое акционерное общество',
-        href: '#b',
+        title: "ПРОИЗВОДСТВО",
+        text: "Работаем с 1939 <br><br> Открытое акционерное общество",
+        href: "#b",
       },
       {
-        title: 'МИССИЯ',
-        text: 'Работаем с 1939 <br><br> Открытое акционерное общество «Кобринский маслодельно-сыродельный завод»',
-        href: '#c',
+        title: "МИССИЯ",
+        text: "Работаем с 1939 <br><br> Открытое акционерное общество «Кобринский маслодельно-сыродельный завод»",
+        href: "#c",
       },
       {
-        title: 'ПОЛИТИКА',
-        text: 'Работаем с 1939 <br><br> <b> Открытое акционерное общество «Кобринский</b> маслодельно-сыродельный завод»',
-        href: '#d',
+        title: "ПОЛИТИКА",
+        text:
+          "Работаем с 1939 <br><br> <b> Открытое акционерное общество «Кобринский</b> маслодельно-сыродельный завод»",
+        href: "#d",
       },
     ];
 
     $aboutPlantContentTitle.html(scheamContetn[0].title);
     $aboutPlantContentText.html(scheamContetn[0].text);
-    $aboutPlantContentLinkHref.attr('href', scheamContetn[0].href)
- 
+    $aboutPlantContentLinkHref.attr("href", scheamContetn[0].href);
+
     linesElem.each((idx, element) => {
-      $(element).on('click', () => {
-        plantScheam.removeClass()
+      $(element).on("click", () => {
+        plantScheam.removeClass();
         plantScheam.addClass(`about-plant__scheam active-section-${idx + 1}`);
         // content
         $aboutPlantContentTitle.html(scheamContetn[idx].title);
         $aboutPlantContentText.html(scheamContetn[idx].text);
-        $aboutPlantContentLinkHref.attr('href', scheamContetn[idx].href);
-      })
+        $aboutPlantContentLinkHref.attr("href", scheamContetn[idx].href);
+      });
     });
 
     // SLIDERS
@@ -165,10 +162,10 @@
           settings: {
             dots: false,
             arrows: false,
-            slidesToShow: 1
-          }
+            slidesToShow: 1,
+          },
         },
-      ]
+      ],
     });
 
     $("#logoSlider").slick({
@@ -183,14 +180,14 @@
             arrows: false,
             slidesToShow: 3,
             slidesToScroll: 3,
-          }
+          },
         },
         {
           breakpoint: 685,
           settings: {
             slidesToShow: 2,
             slidesToScroll: 2,
-          }
+          },
         },
         {
           breakpoint: 520,
@@ -198,9 +195,9 @@
             dots: true,
             slidesToShow: 1,
             slidesToScroll: 1,
-          }
+          },
         },
-      ]
+      ],
     });
 
     $("#sliderProduct").slick({
@@ -214,11 +211,11 @@
           settings: {
             slidesToScroll: 1,
             slidesToShow: 1,
-          }
+          },
         },
-      ]
+      ],
     });
-    
+
     $("#managmentStuff").slick({
       slidesToShow: 3,
       slidesToScroll: 3,
@@ -230,16 +227,16 @@
           settings: {
             slidesToScroll: 2,
             slidesToShow: 2,
-          }
+          },
         },
         {
           breakpoint: 768,
           settings: {
             slidesToScroll: 1,
             slidesToShow: 1,
-          }
+          },
         },
-      ]
+      ],
     });
   });
 })();
